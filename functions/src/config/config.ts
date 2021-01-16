d99db5b2a55e831d0f5c7c3c5df5f2ab99b0c3ed
Alex Patterson <alex@ajonp.com>
2021-01-16 09:20:34 -0500

    add auth token

--- functions/src/config/config.ts
+++ functions/src/config/config.ts
@@ -4,5 +4,7 @@ admin.initializeApp();
 export const firestore = admin.firestore();
 
 // YouTube API Key
-export const youtubeApiKey = process.env.YOUTUBE_API_KEY;
-export const triggerCollection = process.env.TRIGGER_COLLECTION || 'videoMeta';
+export const clientId = process.env.CLIENT_ID;
+export const clientSecret = process.env.CLIENT_SECRET;
+export const tokenCollection = process.env.TOKEN_COLLECTION || 'tokens';
+export const triggerCollection = process.env.TRIGGER_COLLECTION || 'videoMetas';
--- functions/src/youtube/youtube.ts
+++ functions/src/youtube/youtube.ts
@@ -1,14 +1,10 @@
-import { firestore, triggerCollection, youtubeApiKey} from './../config/config';
+import { firestore, triggerCollection } from './../config/config';
 import * as functions from 'firebase-functions';
 import { v4 as uuid } from 'uuid';
 
 export const onVideoMetaCreate = functions.firestore
   .document(`${triggerCollection}/{videoMetaId}`)
   .onCreate(async (snap, context) => {
-    if (!youtubeApiKey) {
-      console.log(`youtubeApiKey missing, please add YOUTUBE_API_KEY`);
-      return;
-    }
 
     const docData = snap.data();
     if (!docData) {
