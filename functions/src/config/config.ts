e0048f687898bd5244e0d6da191cd202de3fed36
Alex Patterson <alex@ajonp.com>
2021-01-16 16:35:25 -0500

    change to youtubeApiTokens

--- functions/src/config/config.ts
+++ functions/src/config/config.ts
@@ -6,5 +6,5 @@ export const firestore = admin.firestore();
 // YouTube API Key
 export const clientId = process.env.CLIENT_ID;
 export const clientSecret = process.env.CLIENT_SECRET;
-export const tokenCollection = process.env.TOKEN_COLLECTION || 'tokens';
+export const tokenCollection = process.env.TOKEN_COLLECTION || 'youtubeApiTokens';
 export const triggerCollection = process.env.TRIGGER_COLLECTION || 'videoMetas';
